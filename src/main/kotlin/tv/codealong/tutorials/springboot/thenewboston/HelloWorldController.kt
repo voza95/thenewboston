c730b76aa7859de917782b7d956420e1daf2b9ea
Vaibhav Oza <35027648+voza95@users.noreply.github.com>
2021-01-19 23:28:46 +0530

    Initial commit

---
+++ src/main/kotlin/tv/codealong/tutorials/springboot/thenewboston/HelloWorldController.kt
@@ -0,0 +1,15 @@
+package tv.codealong.tutorials.springboot.thenewboston
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+    @GetMapping//("/springboot")
+    fun helloWorld(): String = "Hello, This is REST endpoint!"
+
+}
+//Open terminal and type "gradlew build"
